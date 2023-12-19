4e6175ba5cedd8ccfbc2f0ab0ba061a6a5852310
BidhiP <143377352+BidhiP@users.noreply.github.com>
2023-12-19 13:03:22 +0545

    added another array for include

---
+++ IIFE.js
@@ -0,0 +1,40 @@
+(function () {
+    // Static array of names
+    const names = ["Ram", "shyam"];  //names to be displayed in array.
+
+    // Button click event
+    const showNamesButton = document.getElementById("showNamesButton");    //gets data from id "showNamesButton" and put it in ShowNameButton
+    const namesList = document.getElementById("namesList"); //gets data from id "nameLists" and store it on nameList.
+    const addedNames = [];
+    showNamesButton.addEventListener("click", function () {     // Trigger an event on showNameButton when  it is clicked.
+        
+        const uniNames = names.filter(names => !addedNames.includes(names))  //checks if the names are repeated or not and add the other names in it
+        // Display the first two names
+        namesList.innerHTML = "";
+        for (let i = 0; i < uniNames.length; i++) {
+            const li = document.createElement("li");   //creates list element in li
+            li.textContent = uniNames[i];   //gets the content of all element in uniqueNames and put it in li.
+            namesList.appendChild(li); // displays names of names array  by creating a new lists as a child.
+            addedNames.push(uniNames[i]); // pushes the names in addedNames.
+            newNamesList.appendChild(li);  //displays the names written in form by creating a new list as a child
+        }
+        
+    });
+    const nameForm = document.getElementById("nameForm");  // gets data from id "nameForm" and keep it on nameForm
+    const newNameInput = document.getElementById("newName"); // gets data from id "newName" and put it on newNameInput.
+    const newNamesList = document.getElementById("newNamesList"); //gets data from id "newNameList" and put it on newNameList.
+
+    // Form submit event
+
+    nameForm.addEventListener("submit", function (event) {   //event handler for submit button 
+        event.preventDefault();
+        const newName = newNameInput.value;     //the names written in newNameInput will be stored in newName.
+        if (newName) {
+            const li = document.createElement("li"); //creates a list element in li
+            li.textContent = newName;  // gets text from newName.
+            newNamesList.appendChild(li);  // creates a child list 
+            newNameInput.value = "";  // The name is displayed
+        }
+    });
+})();
+
---
+++ index.js
@@ -0,0 +1,97 @@
+//Why IIFE
+(function () {
+    // IIFE is declared and run.
+    const userList = (function () {
+        const DOM = {}
+        const users = [
+            { name: 'digamber', phone: '1234' }
+        ];
+
+        // Array to store names from nameDisplay function
+        const namesArray = [];
+
+        const handleAddNewUserClick = (e) => {
+            // Put your logic for putting list inside array
+            users.push({ name: 'bidhi', phone: '3425' });
+            // Assuming $username is a variable, you can push it to the namesArray
+            namesArray.push($username);
+
+            DOM.userListWrapperEl.innerHTML = '';
+            renderUsers();
+        }
+
+        const eventListeners = () => {
+            // Bail early  
+            if (DOM.container === null) return            
+
+            DOM.addNewUserButtonEl.addEventListener('click', handleAddNewUserClick);
+        }
+
+        const cacheDOM = () => {
+            DOM.container = document.querySelector('.user-list');
+            // Add New Button
+            DOM.addNewUserButtonEl = DOM.container.querySelector('.user-list__add-new-user');
+            // List of users wrapper
+            DOM.userListWrapperEl = DOM.container.querySelector('.user-list__list-of-users');
+        }
+
+        const renderUsers = () => {
+            // Render users from the 'users' array
+            users.forEach((user) => {
+                DOM.userListWrapperEl.innerHTML += `<li class="user-list__add-new-user">${user.name}</li>`;
+            });
+
+            // Render names from the 'namesArray'
+            namesArray.forEach((name) => {
+                DOM.userListWrapperEl.innerHTML += `<li class="user-list__add-new-user">${name}</li>`;
+            });
+        }
+
+        const init = () => {
+            cacheDOM();
+            eventListeners();
+            renderUsers();
+        }
+
+        return { init: init }
+
+    })()
+
+    document.addEventListener('DOMContentLoaded', userList.init);
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const submitButton = document.getElementById('submitButton');
+
+        function nameDisplay(e) {
+            e.preventDefault();
+            const input = document.querySelector('.name');
+            const inputValue = input.value;
+            const todo = document.createElement("li");
+            const list = document.querySelector('.list');
+
+            const closebtn = document.createElement('button');
+            closebtn.textContent = 'x';
+            closebtn.addEventListener('click', function () {
+                list.removeChild(todo);
+            });
+
+            if (inputValue !== '') {
+                todo.textContent = inputValue;
+                todo.appendChild(closebtn);
+                list.appendChild(todo);
+            }
+
+            input.value = '';
+        }
+
+        submitButton.addEventListener('click', nameDisplay);
+
+        clearButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            const list = document.querySelector('.list');
+            list.innerHTML = '';
+        });
+    });
+})();
+
+
